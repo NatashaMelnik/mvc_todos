9d7375e649241b57a9fbbabbe9e5d7f092541e3c
NatashaMelnik <n.melnik1788@gmail.com>
2021-07-30 17:05:43 +0300

    err addtask fix

--- controllers/task.js
+++ controllers/task.js
@@ -11,17 +11,10 @@ class TaskController {
     }
 
     addTask(req, res) {
-        const taskId = parseInt(req.params.id);
-        let task = Task.find(t => +t.id === +taskId);
-        if (task) {
+        let task = { id: Task.length + 1, name: req.body.name };
+        Task.push(task);
+        if (req.body.done === false || req.body.done === true) {
             Object.assign(task, req.body);
-            return res.json(Task);
-        } else {
-            task = { id: Task.length + 1, name: req.body.name };
-            Task.push(task);
-            if (req.body.done === false || req.body.done === true) {
-                Object.assign(task, req.body);
-            }
         }
         return res.send(Task);
     }
