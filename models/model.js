c6eb03df55a7a90513ecda99968e6b51aca52c2d
NatashaMelnik <n.melnik1788@gmail.com>
2021-08-05 12:50:58 +0300

    knex done

--- models/model.js
+++ models/model.js
@@ -1,57 +1,106 @@
 const { Client } = require('pg');
 const config = require('./config');
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'qazpoi',
+        database: 'birthdays'
+    }
+});
+
 class ListTodos {
 
     async checkTaskExistence(listId, id) {
-        const client = new Client(config);
-        client.connect();
-        return client.query('SELECT * FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1 and tasks.id=$2', [+listId, +id])
-            .then(data => {
-                if (data.rows.length > 0) {
+
+        return knex('tasks')
+            .join('lists', 'tasks.list_id', '=', 'lists.id')
+            .select('tasks.id', 'name', 'done')
+            .where({ 'lists.id': +listId, 'tasks.id': +id })
+            .then((data) => {
+                if (data.length > 0) {
                     return true;
                 }
-                else {
-                    return false;
-                }
             });
+
+
+        // const client = new Client(config);
+        // client.connect();
+        // return client.query('SELECT * FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1 and tasks.id=$2', [+listId, +id])
+        //     .then(data => {
+        //         if (data.rows.length > 0) {
+        //             return true;
+        //         }
+        //         else {
+        //             return false;
+        //         }
+        //     });
     }
 
     async displayAll(listId) {
-        const client = new Client(config);
-        client.connect();
-        let list = await client.query('SELECT tasks.id, name, done FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1', [+listId]);
-        return this.clean(list.rows);
+        return knex('tasks')
+            .join('lists', 'tasks.list_id', '=', 'lists.id')
+            .select('tasks.id', 'name', 'done')
+            .where('lists.id', +listId)
+            .then((data) => {
+                return this.clean(data);
+            });
+
+        // const client = new Client(config);
+        // client.connect();
+        // let list = await client.query('SELECT tasks.id, name, done FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1', [+listId]);
+        // return this.clean(list.rows);
     }
 
     async displaySingle(listId, id) {
         return this.checkTaskExistence(listId, id)
             .then(condition => {
                 if (condition) {
-                    const client = new Client(config);
-                    return client.query('SELECT tasks.id, name FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1 and tasks.id=$2', [+listId, +id])
+                    return knex('tasks')
+                        .join('lists', 'tasks.list_id', '=', 'lists.id')
+                        .select('tasks.id', 'name', 'done')
+                        .where({ 'lists.id': +listId, 'tasks.id': +id })
                         .then((data) => {
-                            return data.rows[0];
+                            return this.clean(data);
                         });
+                    // const client = new Client(config);
+                    // return client.query('SELECT tasks.id, name FROM tasks INNER JOIN lists ON tasks.list_id=lists.id where lists.id=$1 and tasks.id=$2', [+listId, +id])
+                    //     .then((data) => {
+                    //         return data.rows[0];
+                    //     });
                 }
             });
     }
 
     async addTask(listId, body) {
-        const client = new Client(config);
-        client.connect();
-        return client.query('INSERT INTO public.tasks(id, name, list_id) VALUES (default, $1, $2);', [body.name, +listId])
+        if (typeof body.done === "boolean") {
+            return knex('public.tasks').insert({ name: body.name, list_id: +listId, done: body.done });
+        }
+        else if (body.name) {
+            return knex('public.tasks').insert({ name: body.name, list_id: +listId });
+        }
+
+        // const client = new Client(config);
+        // client.connect();
+        // return client.query('INSERT INTO public.tasks(id, name, list_id) VALUES (default, $1, $2);', [body.name, +listId])
     }
 
     async updateTask(listId, id, body) {
         return this.checkTaskExistence(listId, id)
             .then((condition) => {
                 if (condition) {
-                    const client = new Client(config);
-                    client.connect();
-                    if (typeof body.done === "boolean") {
-                        return client.query('UPDATE public.tasks SET done=$1 WHERE tasks.id=$2;', [body.done, +body.id])
-                    }
+                    return knex('tasks')
+                        .where('tasks.id', '=', +body.id)
+                        .update({
+                            done: body.done
+                        });
+                    // const client = new Client(config);
+                    // client.connect();
+                    // if (typeof body.done === "boolean") {
+                    //     return client.query('UPDATE public.tasks SET done=$1 WHERE tasks.id=$2;', [body.done, +body.id])
+                    // }
                 }
             });
     }
@@ -60,13 +109,16 @@ class ListTodos {
         return this.checkTaskExistence(listId, id)
             .then((condition) => {
                 if (condition) {
-                    const client = new Client(config);
-                    client.connect();
-                    if (body.name) {
-                        if (typeof body.done === "boolean") {
-                            return client.query('UPDATE public.tasks SET done=$1, name=$2 WHERE tasks.id=$3', [body.done, body.name, +body.id])
-                        }
-                    }
+                    return knex('tasks')
+                        .where('tasks.id', '=', +body.id)
+                        .update(body);
+                    // const client = new Client(config);
+                    // client.connect();
+                    // if (body.name) {
+                    //     if (typeof body.done === "boolean") {
+                    //         return client.query('UPDATE public.tasks SET done=$1, name=$2 WHERE tasks.id=$3', [body.done, body.name, +body.id])
+                    //     }
+                    // }
                 }
             });
     }
@@ -75,11 +127,14 @@ class ListTodos {
         return this.checkTaskExistence(listId, +taskId)
             .then((condition) => {
                 if (condition) {
-                    const client = new Client(config);
-                    client.connect();
-                    if (+taskId) {
-                        return client.query('DELETE FROM public.tasks WHERE tasks.id=$1;', [+taskId])
-                    }
+                    return knex('tasks')
+                    .where('tasks.id', '=', +taskId)
+                    .del();
+                    // const client = new Client(config);
+                    // client.connect();
+                    // if (+taskId) {
+                    //     return client.query('DELETE FROM public.tasks WHERE tasks.id=$1;', [+taskId])
+                    // }
                 }
             })
     }
